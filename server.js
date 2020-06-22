b353ab13e5da6c248c2f0699cfcf470d1a13b875
Pilot <trevorjburni@gmail.com>
2020-06-22 14:07:56 -0600

    created the foundation for the server side code

---
+++ server.js
@@ -0,0 +1,27 @@
+// Require in necessary NPM packages.
+const express = require("express");
+const routes = require("./routes");
+
+// Set the port.
+const PORT = process.env.PORT || 3001;
+
+// Assign express() to app variable.
+const app = express();
+
+// Parse request body as JSON.
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Make a static folder.
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+// Use the routes folder.
+app.use(routes);
+
+// Start the server.
+app.listen(PORT, function () {
+    console.log(`Server now listening on PORT ${PORT}!`);
+});
